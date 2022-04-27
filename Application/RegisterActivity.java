b2ca3f84a3492cfcdc575a2da13e9d0a29d526fd
jeonwonjun <84172525+jeonwonjun@users.noreply.github.com>
2022-04-28 00:15:41 +0900

    Add files via upload

---
+++ Application/RegisterActivity.java
@@ -0,0 +1,77 @@
+package com.example.a409lab_loginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText et_newid, et_newpass, et_newname, et_newage;
+    private Button btn_newregister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) { // 액티비티 시작시 처음으로 실행되는 생명주기!
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        // 아이디 값 찾아주기
+        et_newid = findViewById(R.id.et_newid);
+        et_newpass = findViewById(R.id.et_newpass);
+        et_newname = findViewById(R.id.et_newid);
+        et_newage = findViewById(R.id.et_newage);
+
+        // 회원가입 버튼 클릭 시 수행
+        btn_newregister = findViewById(R.id.btn_register);
+        btn_newregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // EditText에 현재 입력되어있는 값을 get(가져온다)해온다.
+                String userID = et_newid.getText().toString();
+                String userPassword = et_newpass.getText().toString();
+                String userName = et_newname.getText().toString();
+                int userAge = Integer.parseInt(et_newage.getText().toString());
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+                            if (success){ // 회원등록에 성공한 경우
+                                Toast.makeText(getApplicationContext(), "회원 등록에 성공하였습니다.", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                startActivity(intent);
+                            } else{ // 회원등록에 실패한 경우
+                                Toast.makeText(getApplicationContext(), "회원 등록에 실패하였습니다.", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+
+                // 서버로 Volley를 이용해서 요청을 함.
+                RegisterRequest registerRequest = new RegisterRequest(userID, userPassword, userName, userAge, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(registerRequest);
+
+            }
+        });
+
+
+    }
+}
