bf451b44f32fdaac58feac9d63e336be5e35c91f
gayoung lee <gayoung030228@gmail.com>
2022-05-04 18:16:39 +0900

    페이지 전환 기능

--- Application/app/src/main/java/com/example/application_409/MainActivity.java
+++ Application/app/src/main/java/com/example/application_409/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.application_409;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
         // push test
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button Button_add = findViewById(R.id.button2);
+        Button_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button Button_login = findViewById(R.id.button3);
+        Button_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ Application/app/src/main/java/com/example/application_409/RegisterActivity.java
@@ -0,0 +1,13 @@
+package com.example.application_409;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
