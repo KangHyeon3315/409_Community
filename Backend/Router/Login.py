28eef003815b1aeb78e6fbe2a90113ef9927b0a0
kimdongwoo <kimdongwoo0930@gmail.com>
2022-05-05 14:29:56 +0900

    Changes

--- Backend/Router/Login.py
+++ Backend/Router/Login.py
@@ -1,17 +1,107 @@
 import traceback
 import json
-
-from flask import Blueprint, request
+from flask import Flask, Blueprint, request
 
 login_bp = Blueprint('Data', __name__, url_prefix='/api/data')
 
 
-@data_bp.route('/login', methods=('POST',))
+TestName = {}
+TestPassword = {}
+
+
+def ID_CHECK(check):
+    return True
+def PASSWORD_CHECK(check):
+    return True
+
+@login_bp.route('/login', methods=('POST',))
 def login():
     try:
+        if request.method == 'POST':
+            rq_js = request.json(silent=True)
+
+            id = rq_js['id']
+            pw = rq_js['pw']
+
+            if ID_CHECK(id) and PASSWORD_CHECK(pw):  # 아이디 , 비밀번호 체크후 넘기기
+
+                if id in TestPassword.keys():  # 데이터베이스에 아이디가 있는지 확인
+
+                    if TestPassword[id] == pw:  # 유저가 입력한 비밀번호와 데이터베이스의 비밀번호 비교하기
+
+                        return json.dumps({
+                            "Result": True,
+                            "id": id,
+                            "name": TestName[id]
+                        })
+                    else:
+                        return json.dumps({  # 비밀번호 틀림
+                            "Result": False,
+                            "error": "3",
+                            "msg": "wrong password"
+                        })
+                else:
+                    return json.dumps({  # 입력한 아이디가 존재하지 않음
+                        "Result": False,
+                        "error": "2",
+                        "msg": "The id entered does not exist"
+                    })
+            else:
+                return json.dumps({  # 아이디 비번 조건틀림
+                    "Result": False,
+                    "error": "1",
+                    "msg": "Id or password condition is wrong"
+                })
+        else:
+            return json.dumps({  # request is Not POST
+                "Result": False,
+                "error": "0",
+                "msg": "Request is not POST"
+            })
+    except:
+        traceback.print_exc()
+        return json.dumps({"result": False, "error": 0, "msg": traceback.format_exc()})
 
 
-        return json.dumps({"result": True})
+@login_bp.route("/user/register", methods=['POST', 'GET'])
+def user_register():
+    try:
+        if request.method == 'POST':
+
+            rq_js = request.json(silent=True)
+
+            id = rq_js['id']
+            pw = rq_js['pw']
+            name = rq_js['name']  # 유저 닉네임
+            pw_check = rq_js['pw_check']
+
+            # 아이디 체크
+            if ID_CHECK(id) and PASSWORD_CHECK(pw):  # 아이디, 비밀번호 체크후 넘어가기
+
+                if id not in TestPassword.keys():  # 아이디 중복 여부
+
+                    TestName[id] = id  # 유저 이름,유저 아이디 추가
+                    TestPassword[id] = pw  # 유저 아이디에 비밀번호 추가
+
+                    return json.dumps({  # 회원가입 성공
+                        "Result": True,
+                        "name": name
+                    })
+                else:
+                    return json.dumps({  # 이미 존재하는 아이디
+                        "Result": False,
+                        "msg": "The id that already exists"
+                    })
+            else:
+                return json.dumps({  # 아이디 비밀번호 조건 틀림
+                    "Result": False,
+                    "msg": "Id or password condition is wrong"
+                })
+        else:
+            return json.dumps({  # request is Not POST
+                "Result": False,
+                "msg": "Request is not POST"
+            })
     except:
         traceback.print_exc()
         return json.dumps({"result": False, "error": 0, "msg": traceback.format_exc()})
--- Backend/main.py
+++ Backend/main.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask ,request
+from Router.Login import login_bp
+
 
 app = Flask(__name__)
 
 if __name__ == "__main__":
-    from Router.Login import login_bp
+
     app.register_blueprint(login_bp)
 
     app.run(port=7209, debug=False)
