e3c1aed3e54015472b0c73fb46a3e4e3b25a5e5b
YSJ <dbtjwls0112@naver.com>
2022-05-11 16:36:56 +0900

    user check

---
+++ Backend/Utils/Database.py
@@ -0,0 +1,26 @@
+import pymysql
+
+
+class Database:
+
+    def __init__(self):
+        self.connect = pymysql.connect(host="115.23.220.188", user="labs409", password="#@!labs409", db="409Community",
+                                       charset="utf8")
+
+    def is_user(self, user_id, user_pw) -> bool:
+        curs = self.connect.cursor()
+        curs.execute('SELECT * FROM user WHERE id = %s AND pw = %s', (user_id, user_pw,))
+        account = curs.fetchone()
+        if account is None:
+            return False
+        else:
+            return True
+
+    def id_check(self, user_id) -> bool:
+        curs = self.connect.cursor()
+        curs.execute('SELECT * FROM user WHERE id = %s', (user_id,))
+        account = curs.fetchone()
+        if account is None:
+            return True
+        else:
+            return False
