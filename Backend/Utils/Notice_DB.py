adf682427b223bc7126593c867aa671682e75163
YSJ <dbtjwls0112@naver.com>
2022-06-09 16:29:46 +0900

    Database.py Login_DB.py Notice_DB.py

---
+++ Backend/Utils/Notice_DB.py
@@ -0,0 +1,27 @@
+import traceback
+from Backend.Utils.Database import Database
+
+class Notice_DB(Database):
+    def create_notice(self, notice_id, notice_title, notice_writer, notice_date, notice_content) -> bool:
+        try:
+            curs = self.connect.cursor()
+            sql = "INSERT INTO user (id, title, writer, date, content) VALUES (%s %s %s %s)"
+            val = (notice_id, notice_title, notice_writer, notice_date, notice_content)
+            curs.execute(sql, val)
+            self.connect.commit()
+            return True
+        except Exception:
+            traceback.print_exc()
+            return False
+
+    def create_comment(self, comment_id, notice_id, comment_writer, comment_date, parent_id):
+        try:
+            curs = self.connect.cursor()
+            sql = "INSERT INTO user (id, notice_id, writer, date, content) VALUES (%s %s %s %s %s)"
+            val = (comment_id, notice_id, comment_writer, comment_date, parent_id)
+            curs.execute(sql, val)
+            self.connect.commit()
+            return True
+        except Exception:
+            traceback.print_exc()
+            return False
